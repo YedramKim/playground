7e9550bff42d787623efd07da23d50ecc55af12e
YedarmKim <oncoder@naver.com>
2019-03-10 21:13:58 +0900

    eslint 수정 (하지만 실패...)

---
+++ .eslint.vue.js
@@ -0,0 +1,28 @@
+module.exports = {
+	globals: {
+		Vue: true,
+		vue: true,
+		gapi: true
+	},
+	'rules': {
+		'vue/max-attributes-per-line': ['error', {
+			singleline: 3,
+			multiline: {
+				max: 1,
+				allowFirstLine: false
+			}
+		}],
+		'vue/html-indent': ['error', 'tab', {}],
+		'vue/component-name-in-template-casing': ['error', 'kebab-case']
+	},
+	plugins: [
+		'vue'
+	],
+	env: {
+		browser: true
+	},
+	extends: [
+		'plugin:vue/recommended',
+		'./base.js',
+	]
+}

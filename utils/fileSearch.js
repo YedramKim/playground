07b2084c34679a7423b4768e4586056ae9eeb918
YedarmKim <oncoder@naver.com>
2019-03-17 18:01:49 +0900

    filesearch 리팩토링

--- utils/fileSearch.js
+++ utils/fileSearch.js
@@ -3,17 +3,12 @@ const path = require('path');
 
 const fileSearch = async (directoryPath = '', fileRegExp = /\.js$/) => {
 	const fileList = await fse.readdir(directoryPath);
-	// console.info(fileList);
 
 	const resultPromises = fileList.map(async file => {
 		const filePath = path.resolve(directoryPath, file);
 		const stat = await fse.stat(filePath);
 
-		if (stat.isDirectory()) {
-			return fileSearch(filePath, fileRegExp);
-		} else if (fileRegExp.test(file)) {
-			return [filePath];
-		}
+		return stat.isDirectory() ? fileSearch(filePath, fileRegExp) : [filePath];
 	});
 
 	const results = await Promise.all(resultPromises);

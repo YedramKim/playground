80ddd9e8f185729cd1cb8bf018d8714c28a24be4
YedarmKim <oncoder@naver.com>
2019-03-17 18:05:35 +0900

    서버의 라우팅 제작 코드 완료

--- server/index.js
+++ server/index.js
@@ -6,22 +6,32 @@ const {
 	fileSearch,
 } = require('../utils');
 
+const config = {
+	port: 80,
+	middlewares: {
+		pre: [],
+		post: [],
+	},
+};
+
 class Server {
 	constructor() {
 		this.app = express();
+		this.router = express.Router();
 		this.httpServer = http.createServer(this.app);
 
 		this.context = {};
 		this.middlewares = {};
-		this.routes = [];
 	}
 
-	async setupServer() {
-		await this._setupMiddleware();
-		await this._setupRoutes();
+	async _setServer() {
+		await this._setMiddleware();
+		this._setGlobalPreMiddlewares();
+		await this._setRoutes();
+		this._setGlobalPostMiddleware();
 	}
 
-	async _setupMiddleware() {
+	async _setMiddleware() {
 		const middlewarePath = path.resolve(__dirname, 'middleware');
 
 		const middlewarePaths = await fileSearch(middlewarePath);
@@ -37,18 +47,92 @@ class Server {
 		}, {});
 	}
 
-	async _setupRoutes() {
+	_setGlobalPreMiddlewares() {
+		for (const preMiddlewareInfo of config.middlewares.pre) {
+			const preMiddleware = this._getMiddleware(preMiddlewareInfo);
+			this.app.use(preMiddleware);
+		}
+	}
+
+	async _setRoutes() {
 		const routePath = path.resolve(__dirname, 'routes');
 		const routePaths = await fileSearch(routePath);
 
-		this.routes = routePaths.map(routePath => require(routePath));
+		const routeObjects = routePaths.map(routePath => require(routePath));
+
+		for (const routeObject of routeObjects) {
+			this._setRoute(routeObject);
+		}
+		this.app.use(this.router);
 	}
 
-	async listen() {
-		await this._setupServer();
+	_setRoute({
+		path = '',
+		methods,
+		pre = [],
+		handler,
+		post = [],
+	} = {}) {
+		const methodList = ['get', 'post', 'head', 'put', 'delete', 'connect', 'options', 'trace'];
+		methods = Array.isArray(methods) ? methods : [methods];
+
+		if (typeof handler !== 'function') {
+			throw new Error(`${path}의 라우터의 메인 핸들러가 존재하지 않습니다.`);
+		}
+
+		const preMiddlewares = pre.map(middlewareInfo => this._getMiddleware(middlewareInfo));
+		const postMiddlewares = post.map(middlewareInfo => this._getMiddleware(middlewareInfo));
+		const mainHandler = async (req, res, next) => {
+			try {
+				await handler(this.context, req, res);
+			} catch (error) {
+				next(error);
+			}
+		};
+
+		const routeTasks = [
+			...preMiddlewares,
+			mainHandler,
+			...postMiddlewares,
+		];
+
+		for (let method of methods) {
+			method = method.toLowerCase();
+
+			if (methodList.includes(method)) {
+				this.router[method](path, ...routeTasks);
+			} else {
+				throw new Error(`${path}의 라우터에서 올바르지 않은 method를 사용하려고 했습니다.`);
+			}
+		}
+	}
+
+	_setGlobalPostMiddleware() {
+		for (const postMiddlewareInfo of config.middlewares.post) {
+			const postMiddleware = this._getMiddleware(postMiddlewareInfo);
+			this.app.use(postMiddleware);
+		}
+	}
+
+	_getMiddleware(middlewareInfo) {
+		const middlewareData = typeof middlewareInfo === 'string' ? {
+			name: middlewareInfo,
+			data: {},
+		} : middlewareInfo;
+
+		const middlewareConstructor = this.middlewares[middlewareData.name];
+		if (middlewareConstructor) {
+			return middlewareConstructor(middlewareData.data);
+		} else {
+			throw new Error(`${middlewareData.name}은 존재하지 않은 미들웨어 입니다.`);
+		}
+	}
+
+	async startServer() {
+		await this._setServer();
 
 		const httpServerPromise = new Promise(resolve => {
-			this.httpServer.listen(80, () => {
+			this.httpServer.listen(config, () => {
 				console.info('Server running');
 				resolve();
 			});
